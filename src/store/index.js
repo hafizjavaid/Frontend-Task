f757c73d0ec8f4b92581b7babf2d89d9e6fd4be1
hafizjavaid <hafizjaidi786@gmail.com>
2021-06-08 06:10:34 +0500

    Fixing Small UI Issue

--- src/store/index.js
+++ src/store/index.js
@@ -7,21 +7,21 @@ export default new Vuex.Store({
   state: {
     cartItems: [
       {
-        img: require("@/assets/products/item-1.png"),
+        img: require("@/assets/items/item-1.png"),
         title: "Jet Ski",
         id: "434556256",
         price: 1500,
         quantity: 1,
       },
       {
-        img: require("@/assets/products/item-2.png"),
+        img: require("@/assets/items/item-2.png"),
         title: "Bubble Wrap",
         id: "345245865",
         price: 440,
         quantity: 1,
       },
       {
-        img: require("@/assets/products/item-3.png"),
+        img: require("@/assets/items/item-3.png"),
         title: "Crock-Pot",
         id: "987123654",
         price: 55,
